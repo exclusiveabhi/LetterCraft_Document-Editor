4d19ff17996c31fe81b6c0c8a29ff5b8d71afab8
exclusiveabhi <imexclusiveabhi@gmail.com>
2025-03-06 00:36:48 +0530

    final

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    // secure: true, // enable this if using https
+    secure: true, // enable this if using https
     maxAge: 24 * 60 * 60 * 1000 // one day
   }
 }));
